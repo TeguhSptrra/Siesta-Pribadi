7ede42d88a249f6f8b1bdfe6ba8a6548de091d23
TeguhSptrra <102931911+TeguhSptrra@users.noreply.github.com>
2022-10-30 12:29:28 +0700

    Update me.js

--- plugins/userinfo/me.js
+++ plugins/userinfo/me.js
@@ -28,7 +28,7 @@ exports.run = {
          caption += `	◦ *Premium* : ${(user.premium ? '√' : '×')}\n\n`
          caption += global.footer
          client.sendMessageModify(m.chat, caption, m, {
-             title: 'Siesta - Multi Device',
+             title: 'Teguh Bot Premium',
              largeThumb: true,
              thumbnail: pic
          })
