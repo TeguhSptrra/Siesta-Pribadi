efdd123177c4aa3908485aa89e4736f0b8053caa
TeguhSptrra <102931911+TeguhSptrra@users.noreply.github.com>
2022-11-05 20:34:22 +0800

    Update handler.js

--- handler.js
+++ handler.js
@@ -215,7 +215,7 @@ module.exports = async (client, m) => {
                title: 'Maaf Akses Di Tolakk',
                largeThumb: true,
                thumbnail: await Func.fetchBuffer('https://telegra.ph/file/bc0868059809a2d2f39dc.jpg'),
-               url: 'https://chat.whatsapp.com/Fd3dI2zGjOI61Rcn2NLo7A'
+               url: 'https://chat.whatsapp.com/EjpG9d8mwvmBTBw02TvMSt'
             }).then(() => chats.lastchat = new Date() * 1)
             if (setting.self && !['chatAI', 'exec'].includes(name) && !isOwner && !m.fromMe) continue
             if (!m.isGroup && ['chatAI'].includes(name) && body && Func.socmed(body)) continue
