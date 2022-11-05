12ed8a58a772b3e290852223ad85f3c4e4b49ec7
TeguhSptrra <102931911+TeguhSptrra@users.noreply.github.com>
2022-11-05 17:36:55 +0800

    Update client.js

--- client.js
+++ client.js
@@ -162,7 +162,7 @@ const connect = async () => {
          	title: 'Yahh beban grup bertambah',
             largeThumb: true,
             thumbnail: pic,
-            url: 'https://chat.whatsapp.com/Fd3dI2zGjOI61Rcn2NLo7A'
+            url: 'https://chat.whatsapp.com/EjpG9d8mwvmBTBw02TvMSt'
          })
       } else if (room.action == 'remove') {
          let txt = (groupSet.text_left != '' ? groupSet.text_left : text_left).replace('+tag', `@${member.split`@`[0]}`).replace('+grup', `${meta.subject}`)
@@ -170,7 +170,7 @@ const connect = async () => {
             title: 'Sipp beban grup keluar',
             largeThumb: true,
             thumbnail: pic,
-            url: 'https://chat.whatsapp.com/Fd3dI2zGjOI61Rcn2NLo7A'
+            url: 'https://chat.whatsapp.com/EjpG9d8mwvmBTBw02TvMSt'
          })
       }
    })
