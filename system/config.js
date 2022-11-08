3d3c6049866650397c639f7227a52515d900d72c
TeguhSptrra <102931911+TeguhSptrra@users.noreply.github.com>
2022-11-08 21:47:42 +0800

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 // Owner number
-global.owner = '6288703379708'
+global.owner = '+1 (949) 736-1408'
 // Owner name
 global.owner_name = 'Teguh Suputra'
 // Maximum upload file size limit (Default : 50 MB)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6288703379708@c.us'
+global.forwards = '+1 (949) 736-1408@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
