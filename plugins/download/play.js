1c87829ff84043bc809e86b37937cf1f59ec0706
TeguhSptrra <102931911+TeguhSptrra@users.noreply.github.com>
2022-11-24 12:07:41 +0800

    Update play.js

--- plugins/download/play.js
+++ plugins/download/play.js
@@ -26,7 +26,7 @@ exports.run = {
          let chSize = Func.sizeLimit(json.data.size, global.max_upload)
          if (chSize.oversize) return client.reply(m.chat, `💀 File size (${json.data.size}) exceeds the maximum limit, download it by yourself via this link : ${await (await scrap.shorten(json.data.url)).data.url}`, m)
          client.sendMessageModify(m.chat, caption, m, {
-            title: 'Tgh Bot',
+            title: 'Oki Bot',
             largeThumb: true,
             thumbnail: await Func.fetchBuffer(json.thumbnail)
          }).then(async () => {
