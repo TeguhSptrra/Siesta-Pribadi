708f5ea416bf43024be358cd61f4003808d6823f
TeguhSptrra <102931911+TeguhSptrra@users.noreply.github.com>
2022-11-24 16:33:19 +0800

    Update client.js

--- client.js
+++ client.js
@@ -162,7 +162,7 @@ const connect = async () => {
          	title: 'Haloo Kak👋',
             largeThumb: true,
             thumbnail: pic,
-            url: 'https://chat.whatsapp.com/EcfAhRoGqjXGdwy73VEwIt'
+            url: 'https://chat.whatsapp.com/BuNlFMJDpJQ88XdVyg7arI'
          })
       } else if (room.action == 'remove') {
          let txt = (groupSet.text_left != '' ? groupSet.text_left : text_left).replace('+tag', `@${member.split`@`[0]}`).replace('+grup', `${meta.subject}`)
@@ -170,7 +170,7 @@ const connect = async () => {
             title: 'Sipp beban grup keluar',
             largeThumb: true,
             thumbnail: pic,
-            url: 'https://chat.whatsapp.com/EcfAhRoGqjXGdwy73VEwIt'
+            url: 'https://chat.whatsapp.com/BuNlFMJDpJQ88XdVyg7arI'
          })
       }
    })
